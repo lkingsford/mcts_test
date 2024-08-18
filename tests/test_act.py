81f502056c5e47d8853f459bcc065555ac9953ad
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-08-18 14:14:10 +1000

    Some actions not available test

--- tests/test_act.py
+++ tests/test_act.py
@@ -66,12 +66,46 @@ def c4_must_prevent_loss_state():
     return state, 2
 
 
+def c4_some_actions_not_available_state():
+    """
+    Generate a Connect Four game state where the next player (player with ID 1) must
+    make a winning turn, but some columns are full.
+
+    Returns:
+        tuple: A tuple containing the generated game state and the column index where the
+        next player must place their token to win
+    """
+    state = c4.game.GameState(
+        board=np.array(
+            [
+                [1, 2, 1, 0, 0, 1, 0, 0],
+                [2, 1, 2, 0, 0, 2, 0, 0],
+                [2, 1, 2, 0, 0, 1, 2, 0],
+                [1, 1, 2, 0, 1, 2, 2, 0],
+                [1, 2, 1, 0, 1, 2, 2, 0],
+                [1, 1, 2, 2, 2, 1, 1, 0],
+                [2, 2, 2, 1, 1, 2, 1, 0],
+                [2, 1, 2, 1, 1, 2, 1, 0],
+            ]
+        ),
+        next_player_id=0,
+        last_player_id=1,
+        winner=-1,
+        permitted_actions=range(8),
+        previous_actions=[255],
+    )
+    return state, 6
+
+
 @pytest.fixture(
-    scope="module", params=[None, None], ids=["C4: About to win", "C4: Lose unless"]
+    scope="module",
+    params=[None, None, None],
+    ids=["C4: About to win", "C4: Lose unless", "C4: Some actions not available"],
 )
 def c4_states():
     yield c4_about_to_win_state()
     yield c4_must_prevent_loss_state()
+    yield c4_some_actions_not_available_state()
 
 
 def test_act_tree(c4_states):
